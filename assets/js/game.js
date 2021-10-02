4f7089891c3d9e1a939d3cffba681f5dafa0f6ad
Jerome <albert.verdusco@fiscal.treasury.gov>
2021-10-01 21:34:15 -0500

    update

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,7 +24,9 @@ for(var i = 0; i < enemyNames.length; i++) {
 
 // fight function
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+         // Alert players that they are starting the round
     window.alert ("Welcome to Robot Gladiators!");
 
     // ask playerif they'd like to fight or run
@@ -78,7 +80,12 @@ var fight = function(enemyName) {
 
 };
 
+    }
+   
+
 // run fight function to start game
 for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth =50;
+    fight(pickedEnemyName);
 }
